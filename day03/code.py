854f7eab5a3d1c7d70bdb2e83b6b693d6e38bf6c
Petr Svoboda <svobodpe@gmail.com>
2023-12-07 13:57:29 +0100

    day01, day02 from work plus day03 second task

--- day03/code.py
+++ day03/code.py
@@ -18,12 +18,12 @@ test_data = """467..114..
 def split_into_rows(data):
     return data.split("\n")
 
-def get_numbers(data):
-    numbers = []
+def get_matches(data, re_pattern):
+    matches = []
     for i, row in enumerate(split_into_rows(data)):
-        for match in re.finditer(r"(\d+)", row):
-            numbers.append({"number": match.group(), "span": tuple([i]) + match.span()})
-    return numbers
+        for match in re.finditer(re_pattern, row):
+            matches.append({"match": match.group(), "span": tuple([i]) + match.span()})
+    return matches
 
 def check_adjecent_symbol(data, span):
     data = split_into_rows(data)
@@ -41,10 +41,34 @@ def check_adjecent_symbol(data, span):
     print(horizontal_range, vertical_range)
     for i in vertical_range:
         for j in horizontal_range:
-            print(i, j, data[i][j])
-            if data[i][j] not in ".0123456789":
-                return True
+            if i != span[0] or ( i == span[0] and (j == span[1]-1 or j == span[2])):
+                print(i, j, data[i][j])
+                if data[i][j] not in ".0123456789":
+                    return True
     return False
+
+def check_adjecent_numbers(data, numbers, span):
+    output = set()
+    data = split_into_rows(data)
+    row_lenght = len(data[0])
+    column_lenght = len(data)
+    print(row_lenght, column_lenght)
+    horizontal_range = range(
+        span[1]-1 if span[1]-1 >= 0 else 0,
+        span[2]+1 if span[2]+1 <= row_lenght else row_lenght
+        )
+    vertical_range = range(
+        span[0]-1 if span[0]-1 >= 0 else 0,
+        span[0]+2 if span[0]+2 <= column_lenght else column_lenght
+        )
+    print(horizontal_range, vertical_range)
+    for i in vertical_range:
+        row_numbers = [_ for _ in numbers if _["span"][0] == i]
+        for j in horizontal_range:
+            if i != span[0] or ( i == span[0] and (j == span[1]-1 or j == span[2])):
+                print(i, j, data[i][j])
+                output = output.union({ _["match"] for _ in row_numbers if _["span"][1]-1 < j and j < _["span"][2] })
+    return list(output)
 
 
 
@@ -59,17 +83,28 @@ def test_case_1():
     if total == 4361:
         print("test passed")
 
-def main():
+def first_task():
     total = 0
-    for number in get_numbers(data):
+    for number in get_matches(data, r"(\d+)"):
         adjencent = check_adjecent_symbol(data, number["span"])
         print(number, adjencent)
         if adjencent:
-            total += int(number["number"])
+            total += int(number["match"])
     print(f"total: {total}")
+
+def second_task():
+    total = 0
+    numbers = get_matches(data, r"(\d+)")
+    for symbol in get_matches(data, r"(\*)"):
+        adjencent_numbers = check_adjecent_numbers(data, numbers, symbol["span"])
+        print(symbol, adjencent_numbers)
+        if len(adjencent_numbers) == 2:
+            total += int(adjencent_numbers[0])*int(adjencent_numbers[1])
+    print(f"total: {total}")
+
 
 #print(len(split_into_rows(data)))
 
-main()
+second_task()
 
 
