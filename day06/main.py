bd1108830d50522688f3f261cc7174ef073a9bc0
Petr Svoboda <svobodpe@gmail.com>
2023-12-11 18:32:52 +0100

    day06 test_case_one

--- day06/main.py
+++ day06/main.py
@@ -2,10 +2,10 @@ def process_data(file):
     with open(file) as f:
         for row in f:
             if row.startswith("Time:"):
-                time = [int(_) for _ in row.split(":")[1].split()]
+                times = [int(_) for _ in row.split(":")[1].split()]
             elif row.startswith("Distance:"):
-                distance = [int(_) for _ in row.split(":")[1].split()]
-    return time, distance
+                distances = [int(_) for _ in row.split(":")[1].split()]
+    return zip(times, distances)
 
 def distance_formula(time, hold_time):
     return (time-hold_time)*hold_time
@@ -14,8 +14,23 @@ def derivative_distance_formula(time, hold_time):
     return (time-2*hold_time)
 
 def optimum_hold_time(time):
-    return time/2.
+    return time/2
 
-print(optimum_hold_time(7))
-for hold_time in range(8):
-    print(hold_time, distance_formula(7, hold_time), derivative_distance_formula(7, hold_time))
+
+def test_case_one():
+    for time, distance in process_data("test_data.txt"):
+        optimum_hold_time_ = int(optimum_hold_time(time))
+        increment = 0
+        ways_to_win = list()
+        while distance_formula(time, optimum_hold_time_ + increment) > distance:
+            ways_to_win.append(optimum_hold_time_ + increment)
+            increment += 1
+        increment = 0
+        while distance_formula(time, optimum_hold_time_ - increment) > distance:
+            ways_to_win.append(optimum_hold_time_ - increment)
+            increment += 1
+        ways_to_win = set(ways_to_win)
+        print(time, distance, ways_to_win, len(ways_to_win))
+
+
+test_case_one()
