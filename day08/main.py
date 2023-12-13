8dc80bcb6ef3f97e1a7597ef84d55fc549333eae
svobop <61162731+svobop@users.noreply.github.com>
2023-12-13 18:15:40 +0100

    day08 task two wip

--- day08/main.py
+++ day08/main.py
@@ -29,5 +29,21 @@ def task_one():
         elif direction == "R":
             node = network[node][1]
 
+def task_two():
+    instructions, network = process_data("input.txt")
+    nodes = [_ for _ in network.keys() if _[-1] == "A"]
 
-task_one()
+    for steps, direction in enumerate(cycle(instructions)):
+        matches = [_[-1] == "Z" for _ in nodes]
+        if all(matches):
+            print(steps)
+            break
+        elif sum(matches) >= 2:
+            print(steps, nodes, sum(matches))
+        for i, node in enumerate(nodes):
+            if direction == "L":
+                nodes[i] = network[node][0]
+            elif direction == "R":
+                nodes[i] = network[node][1]
+
+task_two()
