17a06b4047713f87db31cdf77b8cbbb1a63de232
svobop <svobodpe@gmail.com>
2023-12-14 17:24:55 +0100

    day08 task two

--- day08/main.py
+++ day08/main.py
@@ -31,16 +31,32 @@ def task_one():
 
 def task_two():
     instructions, network = process_data("input.txt")
-    nodes = [_ for _ in network.keys() if _[-1] == "A"]
-
+    start_nodes = [_ for _ in network.keys() if _[-1] == "A"]
+    nodes = start_nodes.copy()
+    max_matches = 0
+    min_matches = 1
+    len_instructions = len(instructions)
+    z_states = [0 for _ in start_nodes]
+    # print(len_instructions, start_nodes)
     for steps, direction in enumerate(cycle(instructions)):
         matches = [_[-1] == "Z" for _ in nodes]
+        if sum(matches) > max_matches:
+            max_matches = sum(matches)
         if all(matches):
             print(steps)
             break
-        elif sum(matches) >= 2:
-            print(steps, nodes, sum(matches))
+        elif any(matches):
+            z_states = [sum(x) for x in zip(z_states, matches)]
+            print(steps, steps/len_instructions, nodes, sum(matches), max_matches, z_states)
+            if min(z_states) == 2:
+                break
+
+        # elif sum(matches) >= min_matches or steps == 17873:
+        #     print(steps, nodes, sum(matches), max_matches)
+        #     min_matches += 1
         for i, node in enumerate(nodes):
+            # if i % 6 == 0 and node in start_nodes:
+            #     print(steps, nodes, sum(matches), max_matches)
             if direction == "L":
                 nodes[i] = network[node][0]
             elif direction == "R":
